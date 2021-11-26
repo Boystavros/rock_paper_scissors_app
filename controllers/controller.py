5e26bb0e71efc207009117c56d9bcbbfa9f7d6dc
Sacha Ponniah <sponniah812@gmail.com>
2021-11-26 18:06:39 +0000

    First extension complete.

--- controllers/controller.py
+++ controllers/controller.py
@@ -4,9 +4,9 @@ from models.game import Game
 from models.player import Player
 
 
-# @app.route('/')
-# def index():
-#     return render_template('index.html', title="RPS Game")
+@app.route('/')
+def welcome():
+    return render_template('welcome.html', title="Home")
 
 @app.route('/<choice_1>/<choice_2>')
 def index(choice_1, choice_2):
@@ -18,4 +18,4 @@ def index(choice_1, choice_2):
             loser = player_2
     else:
             loser = player_1
-    return render_template('index.html', title="RPS Game", first=player_1, second=player_2, winner=result, loser=loser)
+    return render_template('index.html', title="Outcome", first=player_1, second=player_2, winner=result, loser=loser)
