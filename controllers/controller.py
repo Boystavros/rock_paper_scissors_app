fc7f6f73993c846755f5d4808ee8b4dcb15b1aa0
Sacha Ponniah <sponniah812@gmail.com>
2021-11-26 18:10:43 +0000

    File and route names reorganised.

--- controllers/controller.py
+++ controllers/controller.py
@@ -5,11 +5,11 @@ from models.player import Player
 
 
 @app.route('/')
-def welcome():
-    return render_template('welcome.html', title="Home")
+def index():
+    return render_template('index.html', title="Home")
 
 @app.route('/<choice_1>/<choice_2>')
-def index(choice_1, choice_2):
+def outcome(choice_1, choice_2):
     player_1 = Player("player_1", choice_1)
     player_2 = Player("player_2", choice_2)
     game = Game()
@@ -18,4 +18,4 @@ def index(choice_1, choice_2):
             loser = player_2
     else:
             loser = player_1
-    return render_template('index.html', title="Outcome", first=player_1, second=player_2, winner=result, loser=loser)
+    return render_template('outcome.html', title="Outcome", first=player_1, second=player_2, winner=result, loser=loser)
