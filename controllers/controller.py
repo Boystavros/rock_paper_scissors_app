fb9546792f8f54f113b97a9316239c516fb39612
Sacha Ponniah <sponniah812@gmail.com>
2021-11-26 16:49:00 +0000

    MVP complete.

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,4 +1,3 @@
-from unittest import result
 from flask import render_template
 from app import app
 from models.game import Game
@@ -16,7 +15,7 @@ def index(choice_1, choice_2):
     game = Game()
     result = game.play_game(player_1, player_2)
     if result == player_1:
-        loser = player_2
+            loser = player_2
     else:
-        loser = player_1
-    return render_template('index.html', title="RPS Game", winner=result, loser=loser)
+            loser = player_1
+    return render_template('index.html', title="RPS Game", first=player_1, second=player_2, winner=result, loser=loser)
